4f6dc009a37528e2a9d6919f21aea321382c6f1e
Grant Flannery <grantini@cox.net>
2014-05-18 15:30:34 -0700

    Give move some responsibilities from enemy system more suitable for spawn to allow multiple simultaneous spawns

--- js/dev/engine/level/spawn.js
+++ js/dev/engine/level/spawn.js
@@ -19,12 +19,18 @@ Spawn = function(arrEnemySystems, options) {
 	this.options.positionX 			= options.positionX || 0;
 	this.options.positionY			= options.positionY || 0;
 	this.options.ordinal			= options.ordinal || 0;
-	this.options.hasWarning			= (options.hasWarning === true) ? true : false;
-	this.options.hasSpawnParticle	= (options.hasSpawnParticle === true) ? true : false;
+	this.options.hasWarning			= (options.hasWarning === undefined || options.hasWarning === null) ? false : options.hasWarning;
+	this.options.hasSpawnParticle	= (options.hasSpawnParticle === undefined || options.hasSpawnParticle === null) ? false : options.hasSpawnParticle;
 
 	this.id = -1;
 
+	this.position = null;
+
 	this.enemySystem = null;
+
+	this.currentSpawnedQuantity = 0;
+
+	this.timer = null;
 
 	this.spawnCompleteEvent = new goog.events.Event(EventNames.SPAWN_COMPLETE, this);
 
@@ -40,6 +46,8 @@ Spawn.INTERVAL_THRESHOLD = 1000 / 60;
 */
 Spawn.prototype.init = function() {
 	this.enemySystem = this.arrEnemySystems[this.options.type];
+
+	this.position = new app.b2Vec2();
 
 	goog.events.listen(
 		this.enemySystem,
@@ -64,6 +72,13 @@ Spawn.prototype.clear = function() {
 
 	this.arrEnemySystems = null;
 
+	this.position = null;
+
+	if(this.timer) {
+		clearTimeout(this.timer);
+		this.timer = null;
+	}
+
 	this.options = null;
 
 	this.enemySystem = null;
@@ -74,10 +89,115 @@ Spawn.prototype.clear = function() {
 */
 Spawn.prototype.generate = function() {
 	if(this.options.intervalTime > Spawn.INTERVAL_THRESHOLD) {
-		this.enemySystem.generateByTime(this.options);
+		this.generateEnemyByTime(this.options);
 	} else {
-		this.enemySystem.generate(this.options);
+		this.generateEnemy(this.options);
 		goog.events.dispatchEvent(this.enemySystem, this.enemySystem.spawnCompleteEvent);
+	}
+};
+
+Spawn.prototype.generateEnemy = function(options) {
+	var i = -1,
+		enemy,
+		arrEnemies = [],
+		options = options || {};
+		
+	options.intervalQuantity = options.intervalQuantity || 1;
+	options.posOffsetX 	= options.posOffsetX || 0,
+	options.posOffsetY 	= options.posOffsetY || 0;
+	options.positionX = options.positionX || 0;
+	options.positionY = options.positionY || 0;
+
+	this.position.x = options.positionX;
+	this.position.y = options.positionY;
+
+	while (++i < options.intervalQuantity) {
+		enemy = this.enemySystem.getEnemy();
+		
+		if (enemy) {
+
+			//set enemy position; varies by type
+			if(enemy instanceof EnemyCentipede) {
+				enemy.add();
+
+				enemy.setPosition(
+					new app.b2Vec2(
+						(this.position.x + 
+							Math.randomInRange(-options.posOffsetX, options.posOffsetX)) /
+							app.physicsScale,
+						(this.position.y + 
+							Math.randomInRange(-options.posOffsetY, options.posOffsetY)) /
+							app.physicsScale
+					)
+				);
+			} else {
+				enemy.setPosition(
+					this.position.x + Math.randomInRange(-options.posOffsetX, options.posOffsetX),
+					this.position.y + Math.randomInRange(-options.posOffsetY, options.posOffsetY)
+				);
+			}
+
+			//set target layer for Enemy container; foreground for air, main for ground
+			if(enemy.getCategoryBits() === CollisionCategories.AIR_ENEMY) {
+				app.layers.getStage(LayerTypes.AIR).addChild(enemy.container);
+
+				if(enemy.shadow != null) {
+					app.layers.getStage(LayerTypes.SHADOW).addChild(enemy.shadow.container);
+				}
+			} else {
+				app.layers.getStage(LayerTypes.MAIN).addChild(enemy.container);
+			}
+
+			enemy.setIsAlive(true);
+
+			//notify the wavemanager of this enemy's kill as a default;
+			//this may be changed by other spawn entities outside the general wave spawn scope (eg EnemyCarrier)
+			//as those entities or generators spawn until they are killed
+			enemy.isWaveEnabled = true;
+
+			arrEnemies.push(enemy);
+		}
+	}
+
+	return arrEnemies;
+};
+
+Spawn.prototype.generateEnemyByTime = function(options) {
+	var self = this,
+		options = options || {},
+		quantityDifference;
+
+	options.type 				= options.type;
+	options.intervalTime 		= options.intervalTime || 0;
+	options.intervalQuantity 	= options.intervalQuantity || 1;
+	options.targetQuantity 		= options.targetQuantity || 1;
+
+	var onTimeOut = function () {
+		self.generateEnemy(options);
+
+		if(options.targetQuantity > 0) {
+			self.currentSpawnedQuantity += options.intervalQuantity;
+		}
+
+		if(self.currentSpawnedQuantity < options.targetQuantity) {
+			quantityDifference = options.targetQuantity - self.currentSpawnedQuantity;
+
+			//trim the interval qty to match the target if necessary
+			if(quantityDifference < options.intervalQuantity) {
+				options.intervalQuantity = quantityDifference;
+			}
+
+			self.generateEnemyByTime(options);
+		} else {
+			self.currentSpawnedQuantity = 0;
+			goog.events.dispatchEvent(self.enemySystem, self.enemySystem.spawnCompleteEvent);
+		}
+	};
+
+	if (options.targetQuantity <= this.enemySystem.max || options.intervalQuantity <= this.enemySystem.smax) {
+		this.timer = setTimeout(onTimeOut, options.intervalTime);	
+	} else {
+		throw "Target quantity or interval quantity cannot exceed max quantity.";
 	}
 };
 
